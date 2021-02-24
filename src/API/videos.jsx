a91597a78727627402b74d7c4cda06519ad45d3c
Raphael Caputo <faelelf2@gmail.com>
2021-02-23 23:45:59 -0300

    Comit de deploy

---
+++ src/API/videos.jsx
@@ -0,0 +1,83 @@
+const videos = [
+  {
+    id: 1,
+    title:
+      'RECRIANDO A INTERFACE DO YOUTUBE COM...',
+    channel: 'Lucas Nhimi',
+    views: '1 mi de visualizações',
+    date: 'há 1 dia',
+    avatar: '/imgs/thumb1.png',
+    thumb: '/imgs/thumb1.png',
+  },
+  {
+    id: 2,
+    title:
+      'Eu, a Patroa e as Crianças - temp 3 - Ep- 10',
+    channel: 'neji',
+    views: '1,6 mi visualizações',
+    date: 'há 6 meses',
+    avatar: '/imgs/thumb1.png',
+    thumb: '/imgs/thumb2.png',
+  },
+  {
+    id: 3,
+    title:
+      'Context API vs LocalStorage - Quando...',
+    channel: 'Vinicius Dacal',
+    views: '146 visualizações',
+    date: 'há 11 horas',
+    avatar: '/imgs/avatar.jpeg',
+    thumb: '/imgs/thumb3.png',
+  },
+  {
+    id: 4,
+    title:
+      'GOLAÇOS DE VOLEIO',
+    channel: 'Futebol 24 Horas',
+    views: '57 mil de visualizações',
+    date: 'há 12 horas',
+    avatar: '/imgs/avatar.jpeg',
+    thumb: '/imgs/thumb4.png',
+  },
+  {
+    id: 5,
+    title:
+      'A Primeira Aula de Flutter Que Todo Mundo Deveria...',
+    channel: 'Filipe Deschamps',
+    views: '23 mil de visualizações',
+    date: 'há 1 dia',
+    avatar: '/imgs/avatar.jpeg',
+    thumb: '/imgs/thumb5.png',
+  },
+  {
+    id: 6,
+    title: 'React Hooks useContext Tutorial(Storing a User)',
+    channel: 'Ben Awad',
+    views: '233 mil visualizações',
+    date: 'há 1 semana',
+    avatar: '/imgs/avatar.jpeg',
+    thumb: '/imgs/thumb6.png',
+  },
+  {
+    id: 7,
+    title:
+      'Curso MySQL #01 - O que é um Banco de Dados?',
+    channel: 'Curso em Vídeo',
+    views: '1,3 mi de visualizações',
+    date: 'há 5 anos',
+    avatar: '/imgs/avatar.jpeg',
+    thumb: '/imgs/thumb7.png',
+  },
+  {
+    id: 8,
+    title:
+      'Novo: Marte em 4k',
+    channel: 'ElderFox Documentaries',
+    views: '35 mi de visualizações',
+    date: 'há 7 meses',
+    avatar: '/imgs/avatar.jpeg',
+    thumb: '/imgs/thumb8.png',
+  },
+];
+
+export default videos;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ThemeProvider, createMuiTheme, makeStyles, Button } from '@material-ui/core';
+import Home from '../src/Components/Home';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [aviso, setAviso] = useState(true);
+
+
+
+const handleChange = () => {
+    setAviso(false)
+}
+
+
+  const theme = createMuiTheme({
+    spacing: 4,
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3EA6FF',
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF',
+      },
+    },
+  });
+
+  const useStyles = makeStyles((theme) => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+      
+      {!!aviso ? <div><h1> Atenção, esse site foi feito para estudos!</h1> <Button 
+         variant="contained" color="secondary" disableElevation
+         onClick={() => handleChange()}>Entrar no YouTube/CLone Por Raphael Caputo - </Button> </div> : <Home darkMode={darkMode} setDarkMode={setDarkMode} />}
+      </div>
+    </ThemeProvider>
   );
 }
 
